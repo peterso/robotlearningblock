2d755fd663f04df71f78dc178b0f34d567b75282
Peter So <petermjso@gmail.com>
2020-12-23 12:09:33 +0100

    adapted weight to publish to kaa

---
+++ arduino/KAA_WEIGHT/secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "FRITZ!Box 7530 WE";
+#define SECRET_PASSWORD "70348511462386919316";
+#define SECRET_TOKEN "m5stickc-starter"
+#define SECRET_APP_VERSION "bvgtcvlbhnjfabjf5um0-v1"
