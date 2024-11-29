907f6ae656b12830fda539c24ed86d67e8dbaef4
Pablo Garrido <pablogs9@gmail.com>
2024-11-29 11:16:44 +0100

    Ensure that all Task Status are sent

--- idf/taskboard/main/main_core1.cpp
+++ idf/taskboard/main/main_core1.cpp
@@ -8,6 +8,10 @@
 #include <microros/MicroROSTask.hpp>
 #include <util/Timing.hpp>
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
+
 // Timing configuration for periodic publishers
 constexpr uint32_t TASKBOARD_STATUS_PUBLISHER_PERIOD_MS = 250;
 constexpr uint32_t TASK_STATUS_PUBLISHER_PERIOD_MS = 500;
@@ -55,40 +59,41 @@ void microros_main(
     // ------------------------
     // Task Executor Callback
     // ------------------------
-    // In order to avoid deadlock, let's register a callback to the task executor that defers the
-    // micro-ROS publish operation.
-    TaskHandle_t microros_task_handle = xTaskGetCurrentTaskHandle();
+    // In order to avoid deadlock, the task executor callback will feed a queue of messages to be sent to micro-ROS
+    // This way, the micro-ROS task can handle the messages at its own pace
+    QueueHandle_t task_executor_queue = xQueueCreate(10, sizeof(MicroROSTypes::TaskStatus*));
+
     task_executor.add_callback(
         TASK_STATUS_PUBLISHER_PERIOD_MS,
-        [&microros_task_handle](const Task* task, const Task* precondition)
+        [&task_executor_queue, &task_board_driver](const Task* task, const Task* precondition)
         {
-            // Notify micro-ROS task that a task update is required
-            xTaskNotify(microros_task_handle, MICROROS_TASK_UPDATE_REQUIRED, eSetBits);
-        });
+            // Create a new task status message
+            MicroROSTypes::TaskStatus* task_status = nullptr;
 
-    auto handle_task_executor_notification =
-            [&microros_controller, &task_board_driver](Task* task, Task* precondition)
+            if (nullptr != task)
             {
-                if (nullptr != task)
-                {
-                    // If there's an active task, publish its status
-                    MicroROSTypes::TaskStatus task_status(*task, task_board_driver.get_unique_id());
-                    task_status.set_waiting_precondition(nullptr != precondition);
+                // If there's an active task, publish its status
+                task_status = new MicroROSTypes::TaskStatus(*task, task_board_driver.get_unique_id());
+                task_status->set_waiting_precondition(nullptr != precondition);
 
-                    if (precondition != nullptr)
-                    {
-                        task_status.set_time(0);
-                    }
-
-                    microros_controller.publish_task_status(task_status);
-                }
-                else
+                if (precondition != nullptr)
                 {
-                    // If no active task, publish empty status
-                    MicroROSTypes::TaskStatus empty_task_status(task_board_driver.get_unique_id());
-                    microros_controller.publish_task_status(empty_task_status);
+                    task_status->set_time(0);
                 }
-            };
+            }
+            else
+            {
+                // If no active task, publish empty status
+                task_status = new MicroROSTypes::TaskStatus(task_board_driver.get_unique_id());
+            }
+
+            // If the queue add fails, delete the message
+            if (nullptr != task_status && pdTRUE != xQueueSend(task_executor_queue, &task_status, 0))
+            {
+                ESP_LOGE("microros_main", "Task status queue full, skipping message");
+                delete task_status;
+            }
+        });
 
     // ------------------------
     // Main micro-ROS Update Loop
@@ -100,17 +105,23 @@ void microros_main(
         publish_taskboard_status.update();
         microros_task_executor.update();
 
+        // Consume the task executor queue
+        MicroROSTypes::TaskStatus* task_status = nullptr;
+
+        if (pdTRUE == xQueueReceive(task_executor_queue, &task_status, 0))
+        {
+            if (task_status != nullptr)
+            {
+                microros_controller.publish_task_status(*task_status);
+                delete task_status;
+            }
+        }
+
         // Check for notifications
         uint32_t value;
 
         if (pdPASS == xTaskNotifyWait(pdFALSE, 0xFFFFFFFF, &value, pdMS_TO_TICKS(0)))
         {
-            // Check required update
-            if (value & MICROROS_TASK_UPDATE_REQUIRED)
-            {
-                task_executor.execute_operation_on_task(portMAX_DELAY, handle_task_executor_notification);
-            }
-
             // Check for manual task cancellation
             if (value & MANUALLY_CANCELLED_TASK)
             {
